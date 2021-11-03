6040ffe17104519cd240406706d5ca37072b91bd
fahadsiddqui <fahadsiddqui88@gmail.com>
2021-11-03 14:51:40 +0500

    03-11-2021

---
+++ index.mjs
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express();
+const port = process.env.port || 3000
+
+app.get('/',(req,res)=>{
+    res.send('iam hello world')
+})
+
+app.get('/profile',(req,res)=>{
+    res.send('here is your profile')
+})
+
+app.listen(port,()=>{
+    console.log('server is running')
+})
