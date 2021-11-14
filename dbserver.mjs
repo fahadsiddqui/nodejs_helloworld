acc3246f1fbb341a73d8703bc0c0b906f94f97ce
fahadsiddqui <fahadsiddqui88@gmail.com>
2021-11-14 16:57:17 +0500

    DBserverchanges

--- dbserver.mjs
+++ dbserver.mjs
@@ -1,62 +1,93 @@
 import express from 'express';
-import morgan from 'morgan';
 import cors from 'cors';
+import morgan from 'morgan';
 import mongoose from 'mongoose';
 
-
 //mongoose.connect('mongodb+srv://dbfahad:bharmal786@chatbotalcluster.mpfvc.mongodb.net/mychatapp?retryWrites=true&w=majority');
-mongoose.connect('mongodb+srv://dbfahad:bharmal786@chatbotalcluster.mpfvc.mongodb.net/mychatapp?retryWrites=true&w=majority', function(){
-    console.log('mongodb connected')
-})
-
-const User =mongoose.model('User',{
-    student_name:String,
-    father_name:String,
-    age:String,
-    roll_no:String
+mongoose.connect('mongodb+srv://dbfahad:bharmal786@chatbotalcluster.mpfvc.mongodb.net/mychatapp?retryWrites=true&w=majority',function(){
+  console.log("db Connected");
 });
+
+const User = mongoose.model('users',{
+  student_name:String,
+  father_name:String,
+  age:String,
+  roll_no:String
+});
+
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use(morgan('short'));
 
 app.use((req,res,next)=>{
-    console.log("req come ",req.body);
-    next();
-})
+  console.log("req come");
+  next();
+});
 
-app.get('/users', (req, res) => {
-    User.find({}, (err, users) => {
-      if (!err) {
-        res.send(users)
-      } else {
-        res.status(500).send("error happened")
-      }
-    })  
-})
-
-  app.post('/user', (req, res) => {
-
-    if (!req.body.student_name || !req.body.father_name || !req.body.age) {
-      res.status(400).send("invalid data");
-    } else {
-      const newUser = new User({
-        student_name: req.body.student_name,
-        father_name: req.body.father_name,
-        age: req.body.age,
-        roll_no: req.body.roll_no
-      });
-      newUser.save().then(() => {
-        console.log('user created success')
-        res.send("users created");
-      });
+app.get('/users',(req,res)=>{
+  User.find({},(err,users)=>{
+    if(!err){
+      res.send(users);
+    }else{
+      res.status(500).send("error");
     }
   })
+})
 
+app.get('/user/:id',(req,res)=>{
+  User.findOne({_id:req.params.id},(err,user)=>{
+    if(!err){
+        res.send(user);
+    }else{
+      res.send(500).send("error")
+    }
+  })
+})
+
+app.post('/user',(req,res)=>{
+if(!req.body.student_name ||!req.body.father_name ||!req.body.age ||!req.body.roll_no){
+  res.status(400).send("invalid record");
+}else{
+  const newUser = new User({
+    student_name:req.body.student_name,
+    father_name:req.body.father_name,
+    age:req.body.age,
+    roll_no:req.body.roll_no
+  });
+  newUser.save().then(()=>{
+    console.log("user created");
+    res.send("user created");
+  })
+}
+})
+
+app.put('/user/:id',(req,res)=>{
+let updateUser={};
+if(req.body.student_name){
+  updateUser.student_name = req.body.student_name;
+}
+if(req.body.father_name){
+  updateUser.father_name = req.body.father_name;
+}
+if(req.body.age){
+  updateUser.age = req.body.age;
+}
+if(req.body.roll_no){
+  updateUser.roll_no = req.body.roll_no;
+}
+User.findByIdAndUpdate(req.params.id,updateUser,{new:true},
+  (err,data)=>{
+    if(!err){
+      res.send(data);
+    }else{
+      res.status(500).send("error");
+    }
+  })
+})
 
 
 app.listen(port,()=>{
-    console.log('server is running');
+  console.log("server is running");
 })
-
