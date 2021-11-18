2302aeb7be8026000bf500289203e6d08693b8b7
fahadsiddqui <fahadsiddqui88@gmail.com>
2021-11-18 23:23:41 +0500

    updateEmployee

--- dbserver.mjs
+++ dbserver.mjs
@@ -7,7 +7,7 @@ import mongoose from 'mongoose';
 mongoose.connect('mongodb+srv://dbfahad:bharmal786@chatbotalcluster.mpfvc.mongodb.net/mychatapp?retryWrites=true&w=majority',function(){
   console.log("db Connected");
 });
-
+  
 const User = mongoose.model('student',{
   student_name:String,
   father_name:String,
@@ -17,6 +17,14 @@ const User = mongoose.model('student',{
   email:String,
   gender:String
 });
+
+const Employee = mongoose.model('employee',{
+  student_name:String,
+  father_name:String,
+  age:String,
+  roll_no:String,
+});
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -107,6 +115,78 @@ app.delete('/user/:id', (req, res) => {
   User.findByIdAndRemove(req.params.id, (err, data) => {
     if (!err) {
       res.send("user deleted")
+    } else {
+      res.status(500).send("error happened")
+    }
+  })
+})
+
+//For Employee //
+app.get('/employees',(req,res)=>{
+  Employee.find({},(err,employees)=>{
+    if(!err){
+      res.send(employees);
+    }else{
+      res.status(500).send("error");
+    }
+  })
+})
+
+app.get('/employee/:id',(req,res)=>{
+  Employee.findOne({_id:req.params.id},(err,employee)=>{
+    if(!err){
+        res.send(employee);
+    }else{
+      res.send(500).send("error")
+    }
+  })
+})
+
+app.post('/employee',(req,res)=>{
+if(!req.body.student_name ||!req.body.father_name ||!req.body.age ||!req.body.roll_no){
+  res.status(400).send("invalid record");
+}else{
+  const newEmployee = new Employee({
+    student_name:req.body.student_name,
+    father_name:req.body.father_name,
+    age:req.body.age,
+    roll_no:req.body.roll_no,
+  });
+  newEmployee.save().then(()=>{
+    console.log("Employee created");
+    res.send("Employee created");
+  })
+}
+})
+
+app.put('/employee/:id',(req,res)=>{
+let updateEmployee={};
+if(req.body.student_name){
+  updateEmployee.student_name = req.body.student_name;
+}
+if(req.body.father_name){
+  updateEmployee.father_name = req.body.father_name;
+}
+if(req.body.age){
+  updateEmployee.age = req.body.age;
+}
+if(req.body.roll_no){
+  updateEmployee.roll_no = req.body.roll;
+}
+Employee.findByIdAndUpdate(req.params.id,updateEmployee,{new:true},
+  (err,data)=>{
+    if(!err){
+      res.send(data);
+    }else{
+      res.status(500).send("error");
+    }
+  })
+})
+
+app.delete('/employee/:id', (req, res) => {
+  Employee.findByIdAndRemove(req.params.id, (err, data) => {
+    if (!err) {
+      res.send("Employee deleted")
     } else {
       res.status(500).send("error happened")
     }
