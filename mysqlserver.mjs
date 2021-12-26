2c435a1a31147269e7f53febe84b63538b77e649
Fahad Siddiqui <fahadsiddqui88@gmail.com>
2021-12-26 11:56:56 -0800

    new changes

---
+++ mysqlserver.mjs
@@ -0,0 +1,47 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import mysql from 'mysql';
+
+
+var connection = mysql.createConnection({
+    host     : '173.214.173.246',
+    user     : 'safety53_bsuite',
+    password : 'Bharmal786#',
+    database : 'safety53_db_hacson_2122'
+  });
+
+//   connection.connect(function(err) {
+//     if (err) {
+//       console.error('error connecting: ' + err.stack);
+//       return;
+//     }
+   
+//     console.log('connected as id ' + connection.threadId);
+//   });
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+app.use(morgan('short'));
+
+
+connection.connect(function(err) {
+    if (err) throw err;
+  });
+
+  app.get('/customer/:id' , (req, res) => {
+    connection.query('SELECT * FROM core_customer WHERE customer_id = ?',[req.params.id], (err, rows, fields) => {
+    if (!err)
+    res.send(rows);
+    else
+    console.log(err);
+    })
+    } );
+
+
+app.listen(port,()=>{
+    console.log("server is running");
+  })
